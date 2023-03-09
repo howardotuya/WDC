27e1f5e96643d6baf863fe4c6b39f45ac1d64a7e
Howard Otuya <Howardotuya17@gmail.com>
2023-03-09 01:41:10 +0100

    Changed BrowseRouter to HashRouter for GH

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <Router base="/">
+      <HashRouter base="/">
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
